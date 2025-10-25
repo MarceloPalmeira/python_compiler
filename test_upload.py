4d42efb0f13e83617890c05b1de61d2f583c845a
Marcelo <marcelopalmeira2004@hotmail.com>
2025-10-25 14:41:36 -0300

    refactor: ⚡remocão de testes

--- test_upload.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.post('http://localhost:8000/compiler/upload', 
-                        json={'code': 'var x: number = 10\nprint(x)'})
-print('Status:', response.status_code)
-print('Response:', response.json())
